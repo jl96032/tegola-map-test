c7f5de187dac92fb5b334ce3a8e714d11ee101b3
jl96032 <jl96032@gmail.com>
2022-06-30 11:26:31 +0200

    Initial commit

---
+++ test/tegola-map-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TegolaMapTest from '../lib/tegola-map-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tegola-map-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TegolaMapTest.TegolaMapTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
